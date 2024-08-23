8709834ecbac9fb3726156052fb5a7d0c56508d5
Jonas G. Morsch <jonasgmorsch@gmail.com>
2024-08-23 20:21:41 -0300

    Update Install_Required_Software.ps1

--- Install_Required_Software.ps1
+++ Install_Required_Software.ps1
@@ -4,11 +4,32 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
     exit
 }
 
+function RestartScript()
+{
+    Write-Output "Restarting Script"
+    Sleep 3
+    Start-Process powershell.exe -WindowStyle Maximized -Verb RunAs -ArgumentList ('-noprofile -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))    
+    Exit    
+}
+
 winget list --accept-source-agreements | Out-Null
 
 #windows prompt:
 
-winget install Python.Python.3.9 --scope machine
+winget install Python.Python.3.11 --scope machine
 winget install Kitware.CMake --scope machine
+python.exe -m pip install --upgrade pip
+pip list --outdated | Select-Object -Skip 2 | ForEach-Object { $_.Split(' ')[0] } | ForEach-Object { pip install --upgrade $_ }
+
+try
+{
+    cmake
+}
+catch
+{
+    RestartScript
+}
 
 pip install opencv-python dlib
+
+pause
